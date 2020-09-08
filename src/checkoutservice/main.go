3e8f2deac8190074efb81203459c5f59bb73b8e9
Shayan Hosseini <shayan.hosseiny@gmail.com>
2020-09-09 01:19:01 +0430

    add some real conditions!

--- src/checkoutservice/main.go
+++ src/checkoutservice/main.go
@@ -16,6 +16,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net"
 	"os"
@@ -111,20 +112,39 @@ func main() {
 		Method: pb.CheckoutServiceServer.PlaceOrder,
 		PreConditions: []contracts.Condition{
 			func(req *pb.PlaceOrderRequest) error {
-				log.Info("[PRE] req = ", req)
+				if req.GetCreditCard() == nil {
+					return errors.New("credit card cannot be nil")
+				}
+				year := int32(time.Now().Year())
+				if req.GetCreditCard().GetCreditCardExpirationYear() < year || req.CreditCard.GetCreditCardExpirationYear() > year+4 {
+					log.Info("year = ", year, " req.year = ", req.GetCreditCard().GetCreditCardExpirationYear())
+					return errors.New("credit card year is in invalid range")
+				}
+				if req.CreditCard.GetCreditCardExpirationMonth() > 12 {
+					return errors.New("credit card month is in invalid range")
+				}
+				return nil
+			},
+			func(req *pb.PlaceOrderRequest) error {
+				if req.GetUserId() == "" {
+					return errors.New("user must be authenticated")
+				}
 				return nil
 			},
 		},
 		PostConditions: []contracts.Condition{
+			// Ensure a successful place order call, has a successful ship order call
 			func(resp *pb.PlaceOrderResponse, respErr error, req *pb.PlaceOrderRequest, calls contracts.RPCCallHistory) error {
-				log.Info("[POST] resp = ", resp, "respErr = ", respErr, " req = ", req)
-				log.Info("All()")
-				for _, call := range calls.All() {
-					log.Info(call.MethodName)
+				if respErr != nil {
+					return nil
 				}
-				log.Info("Filter()")
-				for _, call := range calls.Filter(pb.ProductCatalogServiceServer.GetProduct) {
-					log.Info(call.MethodName)
+				shippingCalls := calls.Filter(pb.ShippingServiceClient.ShipOrder)
+				if len(shippingCalls) < 1 {
+					return errors.New("no call to shipping service")
+				}
+				shippingCall := shippingCalls[0]
+				if shippingCall.Error != nil || shippingCall.Response.(*pb.ShipOrderResponse).GetTrackingId() == "" {
+					return errors.New("invalid response from shipping service")
 				}
 				return nil
 			},
